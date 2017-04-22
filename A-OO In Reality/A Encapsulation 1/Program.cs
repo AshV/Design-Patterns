59ad4632537e28f66a1cb39ccbaa3e6f3276fdb3
Ashish Vishwakarma <ashish6318@gmail.com>
2017-04-22 23:12:38 +0530

    Added Encapsulation 1 in OO

---
+++ A-OO In Reality/A Encapsulation 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Encapsulation_1
+{
+    public class Circle
+    {
+        //hide the data. why?
+        //if we change radius to diameter also then clients code will not be impacted
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double area()
+        {
+            return Math.PI * radius * radius;
+        }
+        public double perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //we can create any number of circle type objects using circle class
+            Circle c1 = new Circle(10.2);
+            Console.WriteLine(c1.area());
+            Console.WriteLine(c1.perimeter());
+
+            Circle c2 = new Circle(10.2);
+            Console.WriteLine(c2.area());
+            Console.WriteLine(c2.perimeter());
+        }
+    }
+}
