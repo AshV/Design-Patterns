df16f4af2053bf7d4e58c6bb37c5c9b2907cd22e
Ashish Vishwakarma <ashish6318@gmail.com>
2017-04-22 23:17:54 +0530

    Added Proect for Strategy Pattern

---
+++ D-Strategy Pattern/D-Strategy Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Strategy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
