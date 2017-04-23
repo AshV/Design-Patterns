cffe62b533afd1a869aa8c0069401dafbb580ea9
Ashish Vishwakarma <ashish6318@gmail.com>
2017-04-23 21:24:59 +0530

    Added Solution 1 in Template Method

---
+++ B-Template Pattern/B Solution 1/CoffeeRecipe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Solution_1
+{
+    public class CoffeeRecipe
+    {
+        private void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+        private void addCoffeePowder()
+        {
+            Console.WriteLine("Adding coffee powder");
+        }
+        private void pour()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+        private void addIngradients()
+        {
+            Console.WriteLine("Add milk and sugar");
+        }
+
+        public void makeCoffee()
+        {
+            boilWater();
+            addCoffeePowder();
+            pour();
+            addIngradients();
+        }
+    }
+}
---
+++ B-Template Pattern/B Solution 1/TeaRecipe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Solution_1
+{
+    public class TeaRecipe
+    {
+        private void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+        private void addTeaPowder()
+        {
+            Console.WriteLine("Adding tea powder");
+        }
+        private void pour()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+        private void addIngradients()
+        {
+            Console.WriteLine("Add lemon");
+        }
+
+        public void makeTea()
+        {
+            boilWater();
+            addTeaPowder();
+            pour();
+            addIngradients();
+        }
+    }
+}
