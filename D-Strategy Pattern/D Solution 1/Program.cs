ff1a89d83cb5b3742e0eebf7fd23e81a1ea30fdc
Ashish Vishwakarma <ashish6318@gmail.com>
2017-04-25 20:41:09 +0530

    Strategy Pattern Solution 1 Added

---
+++ D-Strategy Pattern/D Solution 1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace D_Solution_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 6, 7, 8, 3};
+            Sorter sorter = new Sorter(arr);
+            sorter.sort("bubble_sort");
+            sorter.sort("merge_sort");
+        }
+    }
+}
---
+++ D-Strategy Pattern/D Solution 1/Sorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace D_Solution_1
+{
+    public class Sorter
+    {
+        private int[] array;
+
+        public Sorter(int[] arr)
+        {
+            array = arr;
+        }
+
+        private void bubble_sort()
+        {
+            Console.WriteLine("sorting array using bubble sort algorithm");
+        }
+
+        private void merge_sort()
+        {
+            Console.WriteLine("sorting array using merge sort algorithm");
+        }
+
+        public void sort(string algo)
+        {
+            if (algo.Equals("bubble_sort"))
+                bubble_sort();
+            else if (algo.Equals("merge_sort"))
+                merge_sort();
+            else
+                Console.WriteLine("Invalid Sort Algorithm");
+        }
+    }
+}
