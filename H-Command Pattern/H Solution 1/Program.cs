b877af6c944eb128c27834f0133b02782dca8939
Ashish Vishwakarma <ashish6318@gmail.com>
2017-04-26 21:24:13 +0530

    Added Solution 1 in Command Pattern

---
+++ H-Command Pattern/H Solution 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace H_Solution_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square[] squares = new Square[20];
+
+            squares[0] = new Square(0, 10);
+            squares[1] = new Square(1, 15);
+
+            squares[0].move(5, 5);
+            squares[0].scale(2);
+            Console.WriteLine(squares[0].toString());
+
+            squares[1].scale(3);
+            Console.WriteLine(squares[1].toString());
+        }
+    }
+}
---
+++ H-Command Pattern/H-Command Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_Command_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
