db4e1e4bda9bc4964a924107993e3569b7cf69b7
Ashish Vishwakarma <ashish6318@gmail.com>
2017-05-06 19:55:17 +0100

    Added Solution 2 for Adapter Pattern

---
+++ J-Adapter Pattern/J Solution 2/IStack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace J_Solution_2
+{
+    public interface IStack
+    {
+        void push(int e);
+        int? pop();
+        IEnumerator<int> iterator();
+        int size();
+    }
+}
---
+++ J-Adapter Pattern/J Solution 2/LinkedStack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace J_Solution_2
+{
+    public class LinkedStack : IStack
+    {
+        private LinkedList<int> list;
+
+        public LinkedStack()
+        {
+            list = new LinkedList<int>();
+        }
+
+        public IEnumerator<int> iterator()
+        {
+            return list.GetEnumerator();
+        }
+
+        public int? pop()
+        {
+            if (list.Count == 0)
+                return null;
+            int value = list.First.Value;
+            list.RemoveFirst();
+            return value;
+        }
+
+        public void push(int e)
+        {
+            list.AddFirst(e);
+        }
+
+        public int size()
+        {
+            return list.Count;
+        }
+    }
+}
