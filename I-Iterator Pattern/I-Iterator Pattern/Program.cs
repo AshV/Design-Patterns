0c305befd79e127cd455069f28c7dc5813a0704e
Ashish Vishwakarma <ashish6318@gmail.com>
2017-05-06 14:20:07 +0100

    Added project for iterator pattern

---
+++ I-Iterator Pattern/I-Iterator Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Iterator_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
