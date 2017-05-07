3af25204e8462524c8e9992000e0e5c8f2adb300
Ashish Vishwakarma <ashish6318@gmail.com>
2017-05-07 09:35:42 +0100

    Added Folder for Factory Method Pattern

---
+++ L-Factory Method Pattern/L-Factory Method Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_Factory_Method_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
