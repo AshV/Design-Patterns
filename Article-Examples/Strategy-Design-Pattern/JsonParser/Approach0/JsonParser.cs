22de3e811f1a1b3df3a72629ead498c7fb97f8d6
AshV <ashish6318@gmail.com>
2018-04-18 01:17:46 +0530

    Add Approach 0

---
+++ Article-Examples/Strategy-Design-Pattern/JsonParser/Approach0/JsonParser.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+class JsonParser {
+    public object Parse(string source, string jsonData) {
+        WriteLine("Parsing Json Data");
+        // Parsing Logic
+        return new { ParsedData = jsonData };
+    }
+}
---
+++ Article-Examples/Strategy-Design-Pattern/JsonParser/Approach0/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program {
+    static void Main(string[] args) {
+        var pasredData = new JsonParser().Parse(
+            "sensor",
+            "{ 'temp' : '38' }");
+    }
+}
